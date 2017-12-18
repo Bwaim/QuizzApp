dabb54bdd5815d7cadabe380c2b4c6de36d72b5a
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2017-12-18 18:49:17 +0100

    feat: Initialization of the design

--- app/src/main/java/com/bwaim/quizzapp/MainActivity.java
+++ app/src/main/java/com/bwaim/quizzapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.bwaim.quizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // To prevent the Keyboard to appear when the application is launched
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
