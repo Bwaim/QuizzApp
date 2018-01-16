93b75814171bacf538eb2a3905851a3d03482f7a
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-01-16 22:14:23 +0100

    feat: add callback to send button

--- app/src/main/java/com/bwaim/quizzapp/MainActivity.java
+++ app/src/main/java/com/bwaim/quizzapp/MainActivity.java
@@ -1,10 +1,27 @@
 package com.bwaim.quizzapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameEditText;
+    Button sendButton;
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        /**
+         * Callback for the send button
+         * @param view the button clicked
+         */
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
         // To prevent the Keyboard to appear when the application is launched
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        // Get all the needed views
+        usernameEditText = findViewById(R.id.usernameEditText);
+        sendButton = findViewById(R.id.sendButton);
+
+        // Define the callbacks
+        sendButton.setOnClickListener(onClickListener);
     }
+
 }
